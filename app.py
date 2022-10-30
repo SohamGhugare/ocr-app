b42f3cd6bbb3760515352fa549d415aebf15935a
Wendy Edwards <mrscake@gmail.com>
2022-10-30 15:55:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ if button_ingred:
     ingredients = st.text_area('Enter ingredients here', ' ')
     button_gpt = st.button(label = "Analyze")
     if button_gpt:
-    st.write('gpt stuff')
+        st.write('gpt stuff')
     
 
    
